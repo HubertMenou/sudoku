1cb3fded9ae1b9c2b5da77d773c9db5f4753fbe2
HubertMenou <hubert.menou@mines-paristech.fr>
2023-09-25 18:26:15 +0200

    docs: improved cell_groups.py doc

--- sudoku/cell_groups.py
+++ sudoku/cell_groups.py
@@ -19,18 +19,30 @@ class Grouping:
 
 
 class RowGrouping(Grouping):
+    """
+    One group is one row.
+    The parents are the first boxes on the left.
+    """
 
     PARENTS = tuple([(row, 0) for row in range(9)])
     CHILDREN = tuple([(0, col) for col in range(9)])
 
 
 class ColumnGrouping(Grouping):
+    """
+    One group is one column.
+    The parents are the first boxes on the top.
+    """
 
     PARENTS = tuple([(0, col) for col in range(9)])
     CHILDREN = tuple([(row, 0) for row in range(9)])
 
 
 class BoxGrouping(Grouping):
+    """
+    One group is one of the basic 3x3 boxes.
+    The parents are the upper-left boxes.
+    """
 
     PARENTS = tuple([(i, j) for i in [0, 3, 6] for j in [0, 3, 6]])
     CHILDREN = tuple([(i, j) for i in [0, 1, 2] for j in [0, 1, 2]])
