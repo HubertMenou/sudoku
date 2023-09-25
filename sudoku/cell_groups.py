59b0b5ec89f3f357756dd7a1d8d6531800443dcc
HubertMenou <hubert.menou@mines-paristech.fr>
2023-09-25 18:40:17 +0200

    added test samples ABCD

--- sudoku/cell_groups.py
+++ sudoku/cell_groups.py
@@ -4,6 +4,19 @@ from typing import Tuple
 
 
 class Grouping:
+    """
+    In this context:
+        - A group is a set of boxes which should not
+        contain the same value twice.
+        - A parent is one of the box coordinates within a group.
+        - The children are the difference of coordinates describing a group,
+        with respect to a given parent coordinate.
+        - A grouping is a set of groups described as parents/children.
+
+    The parents are described in ARRAY values.
+
+    See the associated example in the RowGrouping docstring.
+    """
 
     PARENTS = None
     CHILDREN = None
@@ -22,6 +35,34 @@ class RowGrouping(Grouping):
     """
     One group is one row.
     The parents are the first boxes on the left.
+
+    Example
+    -------
+
+    +===+===+===+===+===+===+===+===+===+
+    $ A | B | C $ D | E | F $ G | H | J $
+    +---+---+---+---+---+---+---+---+---+
+    $   |   |   $   |   |   $   |   |   $
+    +---+---+---+---+---+---+---+---+---+
+    $   |   |   $   |   |   $   |   |   $
+    +===+===+===+===+===+===+===+===+===+
+    $   |   |   $   |   |   $   |   |   $
+    +---+---+---+---+---+---+---+---+---+
+    $   |   |   $   |   |   $   |   |   $
+    +---+---+---+---+---+---+---+---+---+
+    $   |   |   $   |   |   $   |   |   $
+    +===+===+===+===+===+===+===+===+===+
+    $   |   |   $   |   |   $   |   |   $
+    +---+---+---+---+---+---+---+---+---+
+    $   |   |   $   |   |   $   |   |   $
+    +---+---+---+---+---+---+---+---+---+
+    $   |   |   $   |   |   $   |   |   $
+    +===+===+===+===+===+===+===+===+===+
+
+    The boxes A-B-C-D-E-F-G-H-J belong to the same group.
+    Their parent is A. Described with respect to A, the children
+    description of the group is simply:
+        ((0, 0), (0, 1), ..., (0, 8), (0, 9))
     """
 
     PARENTS = tuple([(row, 0) for row in range(9)])
